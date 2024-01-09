a393969804c352d29912626e6466aeecc358e37e
amas7 <147492063+amas7@users.noreply.github.com>
2024-01-10 00:49:10 +0100

    Update Eater.java
    ajout javadoc

--- src/main/java/org/openjfx/ecosys2/Eater.java
+++ src/main/java/org/openjfx/ecosys2/Eater.java
@@ -1,6 +1,13 @@
 package org.openjfx.ecosys2;
-
+/**
+ * Interface pour les animaux mangeurs. Définit la méthode {@code eat} pour spécifier le comportement alimentaire d'un animal.
+ */
 public interface Eater {
+    /**
+     * Définit le comportement alimentaire d'un animal.
+     *
+     * @param animal L'objet {@code Animal} représentant l'entité à consommer.
+     */
     public void eat(Animal animal);
 }
 
