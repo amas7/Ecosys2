4734afcbdf14c1c9152a4555600356b0a4018e8c
amas7 <147492063+amas7@users.noreply.github.com>
2024-01-10 00:47:55 +0100

    Update Combinaison.java
    modification java doc

--- src/main/java/org/openjfx/ecosys2/Combinaison.java
+++ src/main/java/org/openjfx/ecosys2/Combinaison.java
@@ -8,21 +8,15 @@ import java.util.Random;
  * Elle est utilisée pour générer des combinaisons aléatoires de longueur spécifiée.
  */
 public class Combinaison {
-
-    /** Les caractères possibles pour la génération de combinaisons. */
+//les caracteres utilesé pour genere la comibasion alaetoire
     private static final String charac = "abcdefghijklmnopqrstuvwxyz";
 
-    /** La combinaison générée de caractères aléatoires. */
     private String my_combi;
 
     /** La longueur de la combinaison. */
     private int length;
 
-    /**
-     * Construit une nouvelle Combinaison avec une longueur spécifiée.
-     *
-     * @param length La longueur de la combinaison à générer.
-     */
+
     public Combinaison(int length) {
         this.length = length;
         this.my_combi = this.randomString(this.length);
@@ -46,38 +40,22 @@ public class Combinaison {
         return my_random.toString();
     }
 
-    /**
-     * Obtient la combinaison de caractères aléatoires.
-     *
-     * @return La combinaison de caractères aléatoires.
-     */
+
     public String getMy_combi() {
         return my_combi;
     }
 
-    /**
-     * Définit la combinaison de caractères aléatoires.
-     *
-     * @param my_combi La nouvelle combinaison de caractères aléatoires.
-     */
+
     public void setMy_combi(String my_combi) {
         this.my_combi = my_combi;
     }
 
-    /**
-     * Obtient la longueur de la combinaison.
-     *
-     * @return La longueur de la combinaison.
-     */
+
     public int getLength() {
         return length;
     }
 
-    /**
-     * Définit la longueur de la combinaison.
-     *
-     * @param length La nouvelle longueur de la combinaison.
-     */
+
     public void setLength(int length) {
         this.length = length;
     }
